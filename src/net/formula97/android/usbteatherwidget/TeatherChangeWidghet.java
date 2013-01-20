df28bb096921241c301e0e0ac6033a64cc4a866b
HAJIME Fukuna <f97one@gmail.com>
2013-01-20 11:45:22 +0900

    プロジェクトの初期状態を追加。

---
+++ src/net/formula97/android/usbteatherwidget/TeatherChangeWidghet.java
@@ -0,0 +1,14 @@
+package net.formula97.android.usbteatherwidget;
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * 
+ */
+
+/**
+ * @author kazutoshi
+ *
+ */
+public class TeatherChangeWidghet extends AppWidgetProvider {
+
+}

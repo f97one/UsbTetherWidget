208e246983a5cfd1a0b92b9d22a1cbc8f35f781f
HAJIME Fukuna <f97one@gmail.com>
2013-01-27 10:48:23 +0900

    ウィジェットをタップした時の処理のうち、基本的な部分を作成。

--- src/net/formula97/android/usbteatherwidget/TeatherChangeWidghet.java
+++ src/net/formula97/android/usbteatherwidget/TeatherChangeWidghet.java
@@ -1,14 +1,52 @@
 package net.formula97.android.usbteatherwidget;
-import android.appwidget.AppWidgetProvider;
 
-/**
- * 
- */
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.RemoteViews;
 
 /**
  * @author kazutoshi
  *
  */
 public class TeatherChangeWidghet extends AppWidgetProvider {
+
+	// 自分へのインテントフィルター
+	final String filter = "net.formula97.android.usbteatherwidget.BUTTON_CLICKED";
+
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+			int[] appWidgetIds) {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+		// リモートビューのインスタンスを取得
+		RemoteViews rViews = new RemoteViews(context.getPackageName(), R.layout.layout_1x1);
+
+		Intent intent = new Intent(filter);
+
+		PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+
+		rViews.setOnClickPendingIntent(R.id.ImageButton_WidgetMainBody, pendingIntent);
+
+		appWidgetManager.updateAppWidget(appWidgetIds, rViews);
+	}
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onReceive(context, intent);
+
+		String action = intent.getAction();
+
+		if (action.equals(filter)) {
+			Log.d(context.getPackageName() + "#onReceive", "Button clicked.");
+		}
+	}
+
+
 
 }

47a9da7c00dd9557aeb970b530565f50fb6a9804
HAJIME Fukuna <f97one@gmail.com>
2013-02-15 22:52:57 +0900

    クリック動作の処理をバックグラウンドサービスにするよう、仕様を変更。

--- src/net/formula97/android/usbtetherwidget/TetherChangeWidghet.java
+++ src/net/formula97/android/usbtetherwidget/TetherChangeWidghet.java
@@ -1,22 +1,15 @@
 package net.formula97.android.usbtetherwidget;
 
-import net.formula97.android.usbtetherwidget.R;
-import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
-import android.widget.RemoteViews;
 
 /**
  * @author kazutoshi
  *
  */
 public class TetherChangeWidghet extends AppWidgetProvider {
-
-	// 自分へのインテントフィルター
-	final String filter = "net.formula97.android.usbteatherwidget.BUTTON_CLICKED";
 
 	/* (非 Javadoc)
 	 * @see android.appwidget.AppWidgetProvider#onUpdate(android.content.Context, android.appwidget.AppWidgetManager, int[])
@@ -27,20 +20,6 @@ public class TetherChangeWidghet extends AppWidgetProvider {
 		// TODO 自動生成されたメソッド・スタブ
 		super.onUpdate(context, appWidgetManager, appWidgetIds);
 
-		// リモートビューのインスタンスを取得
-		RemoteViews rViews = new RemoteViews(context.getPackageName(), R.layout.layout_1x1);
-
-		// 自分自身へのインテントインスタンスを生成
-		Intent intent = new Intent(filter);
-
-		// イベント駆動にするための、ペンディングインテントのインスタンスを
-		PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
-
-		// リモートビューのボタンにひもづける
-		rViews.setOnClickPendingIntent(R.id.ImageButton_WidgetMainBody, pendingIntent);
-
-		// ウィジェットを更新する
-		appWidgetManager.updateAppWidget(appWidgetIds, rViews);
 	}
 
 	/* (非 Javadoc)
@@ -51,12 +30,5 @@ public class TetherChangeWidghet extends AppWidgetProvider {
 		// TODO 自動生成されたメソッド・スタブ
 		super.onReceive(context, intent);
 
-		// ウィジェットがタップされた際の挙動
-		//   LogCatに状況を出す
-		String action = intent.getAction();
-
-		if (action.equals(filter)) {
-			Log.d(context.getPackageName() + "#onReceive", "Button clicked.");
-		}
 	}
 }
---
+++ src/net/formula97/android/usbtetherwidget/WidgetManagerServices.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package net.formula97.android.usbtetherwidget;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * @author kazutoshi
+ *
+ */
+public class WidgetManagerServices extends Service {
+
+	/**
+	 * 
+	 */
+	public WidgetManagerServices() {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	/* (非 Javadoc)
+	 * @see android.app.Service#onBind(android.content.Intent)
+	 */
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+}

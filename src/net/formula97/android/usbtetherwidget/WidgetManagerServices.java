c1ddf551117044f2f18ca277a0a23fc3669f4306
HAJIME Fukuna <f97one@gmail.com>
2013-02-17 12:31:16 +0900

    USBテザリングAPI呼び出しとWiMAX制御メソッド呼び出しを、リフレクションで行う処理を追加。
    テストはまだできていない。

--- src/net/formula97/android/usbtetherwidget/WidgetManagerServices.java
+++ src/net/formula97/android/usbtetherwidget/WidgetManagerServices.java
@@ -3,11 +3,16 @@
  */
 package net.formula97.android.usbtetherwidget;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import android.app.PendingIntent;
 import android.app.Service;
 import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.os.IBinder;
 import android.widget.RemoteViews;
 
@@ -88,4 +93,82 @@ public class WidgetManagerServices extends Service {
 		startService(new Intent(this, WidgetManagerServices.class));
 	}
 
+	/**
+	 * USBテザリングAPIを呼び出す。
+	 * 非公開APIをリフレクションで呼び出しているので、動作しない場合もあるかもしれない。
+	 * @return Object型、呼び出しに成功している場合はUSbテザリングAPIのオブジェクトを、失敗している場合はnullを返す。
+	 */
+	@SuppressWarnings("rawtypes")
+	public Object getUsbTetherApi() {
+		Class c = null;
+		Field f = null;
+		Object iconn = null;
+
+		// ConnectivityManagerのサービス一覧を呼び出す
+		ConnectivityManager cm = (ConnectivityManager) getApplicationContext().getSystemService(CONNECTIVITY_SERVICE);
+
+		// CONNECTIVITY_SERVICEのクラスを呼び出す
+		try {
+			c = Class.forName(cm.getClass().getName());
+		} catch (ClassNotFoundException e) {
+			// スタックトレースをLogCatに流す
+			e.printStackTrace();
+		}
+
+		// 呼びだしたクラスからフィールドを取得する
+		try {
+			f = c.getDeclaredField("mService");
+			f.setAccessible(true);
+		} catch (NoSuchFieldException e) {
+			// スタックトレースをLogCatに流す
+			e.printStackTrace();
+		}
+
+		try {
+			iconn = f.get(cm);
+		} catch (IllegalArgumentException e) {
+			// スタックトレースをLogCatに流す
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// スタックトレースをLogCatに流す
+			e.printStackTrace();
+		}
+
+		return iconn;
+	}
+
+	/**
+	 * WiMAX接続のON/OFFを設定する。
+	 * 非公開APIをリフレクションで呼び出しているので、動作しない場合もあるかもしれない。
+	 * なお、WiMAXが使用可能か否かの判定は行っていないが、APIが用意されていない場合は
+	 * NoSuchMethodExceptionが発生するので、一応はわかるかも？
+	 * @param connectionEnabled boolean型、接続をONにするときはtrue、OFFにするときはfalseをセットする。
+	 * @return boolean型、WiMAX接続の切り替えに成功したらtrue、失敗したらfalseを返す。
+	 */
+	public boolean setWimaxConnection(boolean connectionEnabled) {
+		boolean ret = false;
+
+		// WiMAXのサービスを呼び出す
+		Object wimaxmgr = (Object)getBaseContext().getSystemService("wimax");
+
+		// WiMAX接続を切り替える
+		// キャッチ可能な例外発生時は、スタックトレースをLogCatに流す
+		try {
+			Method wimaxEnable = wimaxmgr.getClass().getMethod("setWimaxEnabled", new Class[] { Boolean.TYPE });
+
+			wimaxEnable.invoke(wimaxmgr, new Object[] { connectionEnabled });
+
+			ret = true;
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
+
+		return ret;
+	}
 }

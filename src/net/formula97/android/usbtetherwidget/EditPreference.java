3a28395beacc510451e673004b331b63afc4450c
HAJIME Fukuna <f97one@gmail.com>
2013-03-16 14:26:15 +0900

    PreferenceActivity#addPreferencefromResourceがAPI Level 11からDeprecatedになっているので、PreferenceFragmentを使うようにするため内部クラスを定義してaddPreferenceFromResourceの呼び出し場所を移動した。

--- src/net/formula97/android/usbtetherwidget/EditPreference.java
+++ src/net/formula97/android/usbtetherwidget/EditPreference.java
@@ -5,6 +5,7 @@ package net.formula97.android.usbtetherwidget;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 
 /**
  * @author kazutoshi
@@ -19,7 +20,32 @@ public class EditPreference extends PreferenceActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO 自動生成されたメソッド・スタブ
 		super.onCreate(savedInstanceState);
-		addPreferencesFromResource(R.xml.settings_main);
+
+		// PreferenceActivity#addPreferencefromResourceがAPI Level 11から
+		// Deprecatedになっていることへの対策
+		//addPreferencesFromResource(R.xml.settings_main);
+		getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
 	}
 
+	/**
+	 * 手っ取り早い対策として、PreferenceFragmentを継承したfragmentクラスを
+	 * 作成した。
+	 * @author kazutoshi
+	 * @see http://stackoverflow.com/questions/6822319/what-to-use-instead-of-addpreferencesfromresource-in-a-preferenceactivity
+	 */
+	public static class MyPreferenceFragment extends PreferenceFragment {
+
+		/* (非 Javadoc)
+		 * @see android.preference.PreferenceFragment#onCreate(android.os.Bundle)
+		 */
+		@Override
+		public void onCreate(final Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+
+            // PreferenceFragment#addPreferencesFromResourceでプリファレンスを
+            // 呼び出す
+            addPreferencesFromResource(R.xml.settings_main);
+        }
+	}
 }
